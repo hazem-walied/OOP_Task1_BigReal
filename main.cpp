fa329fc8785522f4f767a8793b7160a90a0c99f2
Ethar1Raid <ethar.raid@yahoo.com>
2022-11-07 22:25:04 +0200

    added examples

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ using namespace std;
 
 int main()
 {
-    BigReal n1 ("211.1");
-    BigReal n2 ("1.031");
-    BigReal n4 = n1 - n2;
-    //should print result 221.21343222
-    cout << n4 << endl;
+    BigReal n1 ("11.90");
+    BigReal n2 ("233.1134322222222292");
+    BigReal n3 = n1 - n2;
+    //next statement will print -221.21134322222222292
+    cout << n3 << endl;
+    //next statement will print 1
+    cout << n3.sign() << endl;
+
+    BigReal p1 ("11.90");
+    BigReal p2 ("233.1134322222222292");
+    BigReal p3 = p1 + p2;
+    //next statement will print 245.01
+    cout << p3 << endl;
+    cout << "size of p3 is: " << p3.size() << endl;
+
+    //input assignment overloading
+    cin >> n1;
+    cout << n1 << endl;
     return 0;
 }
 
