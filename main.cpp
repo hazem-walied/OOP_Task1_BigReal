36a81bee9570e4b5be1326204c2cab97fe81d42a
Ziad Ahmed <ziad.a200343@gmail.com>
2022-11-08 06:50:46 +0200

    Fixing some bugs in the plus operator function

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 
 int main()
 {
-    BigReal n1 ("211.1");
-    BigReal n2 ("1.031");
-    BigReal n4 = n1 - n2;
+    BigReal n1 ("10.5");
+    BigReal n2 ("0.5");
+    BigReal n4 = n1 + n2;
     //should print result 221.21343222
     cout << n4 << endl;
     return 0;
